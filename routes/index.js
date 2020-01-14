384cfbd91ba8f458e628df6eff6eccfa84bdd5a3
Jaeseok Yoo <opp0615@naver.com>
2020-01-14 16:44:53 +0900

    data fix and filter button

--- routes/index.js
+++ routes/index.js
@@ -12,15 +12,8 @@ var readXlsxFile = require('read-excel-file/node')
 router.get('/', function(req, res, next) {
   var obj = {}
 
-  readXlsxFile('./data.xlsx').then((rows) =>{
+  readXlsxFile('./config/data.xlsx').then((rows) =>{
     obj = {"data" : rows};
-    for(var i in rows){
-      if(rows[i][4]){
-        console.log("구장명 : "+rows[i][3]);
-        console.log("주소 : "+rows[i][4]);
-      }
-    }
-
 
     res.render('index.ejs', obj);
   })
